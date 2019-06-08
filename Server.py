c6706e6183806b2320d42e8af5f4914bc85a403e
WimpPgK <44860992+WimpPgK@users.noreply.github.com>
2019-06-08 23:08:29 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,3 @@
-# RPC polega na zdalnym wywolywaniu procedur. Zaden kod programu nie jest przesylany przez siec.
-# Na serwerze sa zdefiniowane normalne funkcje tak jak w zwyklym programie z tą roznica, ze
-# kazda funkcja zaczynajaca sie od exposed moze zostac wywolana przez klienta
-# wynik funkcji zwracany jest przez return.
-
-
 import rpyc
 from rpyc.utils.server import ThreadedServer
 
